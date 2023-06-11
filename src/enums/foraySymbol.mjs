80309749ecbba08cf5c9793a7748da29fe692038
spiraldust-games <133811357+spiraldust-games@users.noreply.github.com>
2023-06-11 21:30:14 +0100

    add enums folder

---
+++ src/enums/foraySymbol.mjs
@@ -0,0 +1,4 @@
+// used to attach the array to our returned API object
+const foraySymbol = Symbol('foray');
+
+export default foraySymbol;
---
+++ src/enums/hookCategory.mjs
@@ -0,0 +1,6 @@
+export default Object.freeze({
+  AFTER: 'after',
+  AFTER_ALL: 'afterAll',
+  BEFORE: 'before',
+  BEFORE_ALL: 'beforeAll',
+});

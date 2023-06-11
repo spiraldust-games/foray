0b1f422197bb1da7cc8a85de59cdf63e9edd82ec
spiraldust-games <133811357+spiraldust-games@users.noreply.github.com>
2023-06-11 21:31:19 +0100

    add operators folder

---
+++ src/operators/cohesive.mjs
@@ -0,0 +1,14 @@
+/**
+ * Combine more than one atom together.
+ * @param {...function} atoms
+ */
+export default function cohesive(...atoms) {
+  return function _cohesive(item) {
+    return atoms.reduce((result, atomFn) => {
+      const output = atomFn.call(this, result);
+      this.setOutput(output);
+
+      return output;
+    }, item);
+  };
+}

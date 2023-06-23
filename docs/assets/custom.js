16d5e8261a63652da0e2d1fed80b65cec3405ba3
spiraldust-games <133811357+spiraldust-games@users.noreply.github.com>
2023-06-23 11:18:22 +0100

    improve documentation

--- docs/assets/custom.js
+++ docs/assets/custom.js
@@ -1,6 +1,12 @@
 const query = document.getElementsByClassName('documentation');
+const intro = document.querySelector('.keyline-top-not.py2');
 const doc = document.documentElement;
 const body = query && query[0];
+const r = (v) => (Math.round((v*1000))/1000);
+
+if (intro && intro.classList) {
+  intro.classList.add('intro');
+}
 
 window.CSS.registerProperty({
   name: "--starfieldStars",
@@ -20,8 +26,10 @@ CSS.paintWorklet.addModule('./assets/starfield-worklet.js');
 
 if (body) {
   const m = 500;
+  let switched = 0;
   let offset = 0;
   let dir = 1;
+  let pdir = 1;
   let target = 1;
 
   function update() {
@@ -31,13 +39,25 @@ if (body) {
     else if (offset < -m) {
       target = 1;
     }
+    pdir = dir;
     if (dir < target) {
       dir += 0.01;
     }
     else if (dir > target) {
       dir -= 0.01;
     }
-    doc.style.setProperty('--starfieldOffset', offset += dir);
+
+    if (pdir > 0 && dir < 0) {
+      switched++;
+    }
+    if (pdir < 0 && dir > 0) {
+      switched++;
+    }
+    if (switched > 2) return;
+
+    offset += dir;
+
+    doc.style.setProperty('--starfieldOffset', r(offset));
     body.style.backgroundColor = offset & 2 ? '#272730' : '#272731';
 
     requestAnimationFrame(update);

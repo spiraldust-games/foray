860b2bc05dd2c1eec20a7abfc9b37a4affefcb35
spiraldust-games <133811357+spiraldust-games@users.noreply.github.com>
2023-07-07 07:09:13 +0100

    make sure makeWithArg does not constantly recreate

--- src/initialisers/makeWithArg.mjs
+++ src/initialisers/makeWithArg.mjs
@@ -1,13 +1,26 @@
 /**
  * This is an example of an atom initialiser that will initialise the atom
- * during the runtime of the fn().
+ * during the runtime of the `fn()`. Once the atom function has been initialised
+ * it will not be re-initialised, unless the argument passed during
+ * initialisation changes value.
  * @param {function} uninitialisedAtomFn
  * @param {number} offset
  */
 export default function makeWithArg(uninitialisedAtomFn, offset) {
+  let lastArg;
+  let lastFn;
+
   return function _makeWithArg(item) {
     const arg = this.getArg(offset);
+
+    if (arg === lastArg && lastFn) {
+      return lastFn.call(this, item);
+    }
+
     const atomFnLive = uninitialisedAtomFn.call(this, arg);
+
+    lastArg = arg;
+    lastFn = atomFnLive;
 
     return atomFnLive.call(this, item);
   };
